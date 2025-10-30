16c6652687efd00bb7e437397c1f8d34b5f0c102
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 13:01:16 -0400

    adding ORDERSERVICE222

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,4 @@
-def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG, String DOCKERHUB_USER) {
-    // Build and tag for local use
+def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG) {
     sh "docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/"
     sh "docker build -t ${ORDERSERVICE}:${IMAGE_TAG} order-service/"
-    
-    // Tag for Docker Hub
-    sh """
-        docker tag ${FRONTEND}:${IMAGE_TAG} ${DOCKERHUB_USER}/${FRONTEND}:${IMAGE_TAG}
-        docker tag ${ORDERSERVICE}:${IMAGE_TAG} ${DOCKERHUB_USER}/${ORDERSERVICE}:${IMAGE_TAG}
-    """
 }
