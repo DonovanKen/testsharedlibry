46c6170a0e94e59651df7a53884eeb669f55657a
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 11:50:20 -0400

    adding ORDERSERVICE

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,5 @@
-def call(String FRONTEND, String IMAGE_TAG) {
+def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG) {
     sh 'docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/'
+    sh 'docker build -t ${ORDERSERVICE}:${IMAGE_TAG} orderservice/'
 }
 
-def call(String ORDERSERVICE, String IMAGE_TAG) {
-    sh 'docker build -t ${ORDERSERVICE}:${IMAGE_TAG} frontend/'
-}
