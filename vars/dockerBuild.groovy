fd6cea3079c5c7a6b9e8a0d20c47b59580902a36
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 12:45:59 -0400

    adding ORDERSERVICE22222222222222

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG) {
     // Use double quotes for variable interpolation
     sh "docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/"
-    sh "docker build -t ${ORDERSERVICE}:${IMAGE_TAG} orderservice/"
+    sh "docker build -t ${ORDERSERVICE}:${IMAGE_TAG} order-service/"
 }
