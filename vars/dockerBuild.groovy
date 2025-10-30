688f8beec69aac72f4d95724ccc55bedad76852b
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 12:01:36 -0400

    adding ORDERSERVICE

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,4 @@
-def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG) {
-    sh 'docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/'
-    sh 'docker build -t ${ORDERSERVICE}:${IMAGE_TAG} orderservice/'
+def call(String DOCKERHUB_USER, String FRONTEND, String ORDERSERVICE, String IMAGE_TAG) {
+    sh "docker build -t ${DOCKERHUB_USER}/${FRONTEND}:${IMAGE_TAG} frontend/"
+    sh "docker build -t ${DOCKERHUB_USER}/${ORDERSERVICE}:${IMAGE_TAG} orderservice/"
 }
-
--- vars/testimage.groovy
+++ vars/testimage.groovy
@@ -1,34 +1,33 @@
 def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG, String CONTAINER_FRONTEND, String CONTAINER_ORDERSERVICE) {
-    sh '''
-                   echo "delete old container and create new..."
-                   docker rm -f ${CONTAINER_FRONTEND}
-                   docker run -d -p 5000:5000 --name ${CONTAINER_FRONTEND} ${FRONTEND}:${IMAGE_TAG}
-                   docker ps
-                   sleep 5
+    // Test frontend
+    sh """
+        echo "Testing frontend container..."
+        docker rm -f ${CONTAINER_FRONTEND} || true
+        docker run -d -p 5000:5000 --name ${CONTAINER_FRONTEND} ${FRONTEND}:${IMAGE_TAG}
+        docker ps
+        sleep 5
 
-                   echo "testing container..."
-                   curl -I http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_FRONTEND}):5000
+        echo "Testing frontend container..."
+        curl -I http://\$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_FRONTEND}):5000
 
-                   echo "delete tested container..."
-                   docker rm -f ${CONTAINER_FRONTEND}
+        echo "Cleaning up frontend container..."
+        docker rm -f ${CONTAINER_FRONTEND}
+        echo "Frontend test completed..."
+    """
 
-                    echo "End..."
-                '''
+    // Test orderservice  
+    sh """
+        echo "Testing orderservice container..."
+        docker rm -f ${CONTAINER_ORDERSERVICE} || true
+        docker run -d -p 5003:5003 --name ${CONTAINER_ORDERSERVICE} ${ORDERSERVICE}:${IMAGE_TAG}
+        docker ps
+        sleep 5
 
+        echo "Testing orderservice container..."
+        curl -I http://\$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_ORDERSERVICE}):5003
 
-    sh '''
-                   echo "delete old container and create new..."
-                   docker rm -f ${CONTAINER_ORDERSERVICE}
-                   docker run -d -p 5000:5000 --name ${CONTAINER_ORDERSERVICE} ${ORDERSERVICE}:${IMAGE_TAG}
-                   docker ps
-                   sleep 5
-
-                   echo "testing container..."
-                   curl -I http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_ORDERSERVICE}):5003
-
-                   echo "delete tested container..."
-                   docker rm -f ${CONTAINER_ORDERSERVICE}
-
-                    echo "End..."
-                '''
+        echo "Cleaning up orderservice container..."
+        docker rm -f ${CONTAINER_ORDERSERVICE}
+        echo "Orderservice test completed..."
+    """
 }
