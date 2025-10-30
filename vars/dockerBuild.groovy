422f7d760ce32464f954fcbc499947bfe0ba4ec8
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 14:00:51 -0400

    adding userservice

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,6 @@
-def call(String FRONTEND, String ORDERSERVICE, String PRODUCTSERVICE, String IMAGE_TAG) {
+def call(String FRONTEND, String ORDERSERVICE, String PRODUCTSERVICE, String USERSERVICE, String IMAGE_TAG) {
     sh "docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/"
     sh "docker build -t ${ORDERSERVICE}:${IMAGE_TAG} order-service/"
     sh "docker build -t ${PRODUCTSERVICE}:${IMAGE_TAG} product-service/"
+    sh "docker build -t ${USERSERVICE}:${IMAGE_TAG} user-service/"
 }
--- vars/testimage.groovy
+++ vars/testimage.groovy
@@ -1,4 +1,4 @@
-def call(String FRONTEND, String ORDERSERVICE, String PRODUCTSERVICE, String IMAGE_TAG, String CONTAINER_FRONTEND, String CONTAINER_ORDERSERVICE, String CONTAINER_PRODUCTSERVICE) {
+def call(String FRONTEND, String ORDERSERVICE, String PRODUCTSERVICE, String USERSERVICE, String IMAGE_TAG, String CONTAINER_FRONTEND, String CONTAINER_ORDERSERVICE, String USERSERVICE_PRODUCTSERVICE, ) {
     // Test frontend
     sh """
         echo "Testing frontend container..."
@@ -39,5 +39,19 @@ def call(String FRONTEND, String ORDERSERVICE, String PRODUCTSERVICE, String IMA
         echo "Cleaning up productservice container..."
         docker rm -f ${CONTAINER_PRODUCTSERVICE}
         echo "Productservice test completed..."
+    """
+
+    // Test userservice
+    sh """
+        echo "Testing userservice container..."
+        docker rm -f ${CONTAINER_USERSERVICE} || true
+        docker run -d -p 5001:5001 --name ${CONTAINER_USERSERVICE} ${USERSERVICE}:${IMAGE_TAG}
+        docker ps
+        sleep 5
+        echo "Testing userservice container..."
+        curl -I http://\$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_USERSERVICE}):5001 || true
+        echo "Cleaning up userservice container..."
+        docker rm -f ${CONTAINER_USERSERVICE}
+        echo "userservice test completed..."
     """
 }
