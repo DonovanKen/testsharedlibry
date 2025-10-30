361fcf1790e854d47dd6e197061cedac38cb604f
DonovanKen <donovankengiseh@gmail.com>
2025-10-30 11:42:34 -0400

    adding ORDERSERVICE

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,7 @@
 def call(String FRONTEND, String IMAGE_TAG) {
     sh 'docker build -t ${FRONTEND}:${IMAGE_TAG} frontend/'
 }
+
+def call(String ORDERSERVICE, String IMAGE_TAG) {
+    sh 'docker build -t ${ORDERSERVICE}:${IMAGE_TAG} frontend/'
+}
--- vars/testimage.groovy
+++ vars/testimage.groovy
@@ -1,16 +1,33 @@
-def call(String FRONTEND, String IMAGE_TAG, String CONTAINER_NAME) {
+def call(String FRONTEND, String ORDERSERVICE, String IMAGE_TAG, String CONTAINER_FRONTEND, String CONTAINER_ORDERSERVICE) {
     sh '''
                    echo "delete old container and create new..."
-                   docker rm -f ${CONTAINER_NAME}
-                   docker run -d -p 5000:5000 --name ${CONTAINER_NAME} ${FRONTEND}:${IMAGE_TAG}
+                   docker rm -f ${CONTAINER_FRONTEND}
+                   docker run -d -p 5000:5000 --name ${CONTAINER_FRONTEND} ${FRONTEND}:${IMAGE_TAG}
                    docker ps
                    sleep 5
 
                    echo "testing container..."
-                   curl -I http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME}):5000
+                   curl -I http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_FRONTEND}):5000
 
                    echo "delete tested container..."
-                   docker rm -f ${CONTAINER_NAME}
+                   docker rm -f ${CONTAINER_FRONTEND}
+
+                    echo "End..."
+                '''
+
+
+    sh '''
+                   echo "delete old container and create new..."
+                   docker rm -f ${CONTAINER_ORDERSERVICE}
+                   docker run -d -p 5000:5000 --name ${CONTAINER_ORDERSERVICE} ${ORDERSERVICE}:${IMAGE_TAG}
+                   docker ps
+                   sleep 5
+
+                   echo "testing container..."
+                   curl -I http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_ORDERSERVICE}):5003
+
+                   echo "delete tested container..."
+                   docker rm -f ${CONTAINER_ORDERSERVICE}
 
                     echo "End..."
                 '''
